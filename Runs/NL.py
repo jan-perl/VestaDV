50656954cc4553ce8ac6f2fd64a70c4db6eb7c05
basvanbemmel <bas.vanbemmel@pbl.nl>
2016-12-21 12:38:11 +0100

    python-files: genereren merge-files van csv-vesta-resultaten tevens batch-files tbv aanroep

---
+++ Runs/NL.py
@@ -0,0 +1,8 @@
+# Sortlist item has for fields: first field: orderingsnumber, second: filename,
+#      third = True: file must exist, False: file may not be there.
+#      Fourth the number of lines in the output
+sortlist = [\
+[1,"NL\Co2Flow.csv",True,16],\
+[2,"NL\CostsFlow.csv",True,16],\
+[3,"NL\EnergyFlow.csv",True,16]\
+]
---
+++ Runs/mut_bebouwing_allstock.py
@@ -0,0 +1,18 @@
+# Sortlist item has for fields: first field: orderingsnumber, second: filename,
+#      third = True: file must exist, False: file may not be there.
+#      Fourth the number of lines in the output
+sortlist = [\
+[1,"AllStock\mut_Glastuinbouw\Eigenaar.csv",True,23],\
+[2,"AllStock\mut_Glastuinbouw\Gebruiker.csv",False,19],\
+[3,"AllStock\mut_UitlegGlTb\Eigenaar.csv",True,23],\
+[4,"AllStock\mut_UitlegGlTb\Gebruiker.csv",False,19],\
+[5,"AllStock\mut_UitlegUtil\Eigenaar.csv",True,23],\
+[6,"AllStock\mut_UitlegUtil\Gebruiker.csv",False,19],\
+[7,"AllStock\mut_UitlegWoning\Eigenaar.csv",True,33],\
+[8,"AllStock\mut_UitlegWoning\Gebruiker.csv",False,31],\
+[9,"AllStock\mut_Utiliteit\Eigenaar.csv",True,23],\
+[10,"AllStock\mut_Utiliteit\Gebruiker.csv",False,19],\
+[11,"AllStock\mut_Woning\Eigenaar.csv",True,34],\
+[12,"AllStock\mut_Woning\Gebruiker.csv",False,32]\
+]
+
