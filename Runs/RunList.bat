81910ad0e9f38885f600093f6b8c994b2a19b634
basvanbemmel <bas.vanbemmel@pbl.nl>
2017-01-11 11:15:35 +0100

    resutaat runs met github-verise 540ee8 en Sourcedata op ftp 7.137.exe versie2
    zip-bestanden verwijderd niet direct toepasbaar
    Verhoging50cent-bestanden, tevens in de runlist

--- Runs/RunList.bat
+++ Runs/RunList.bat
@@ -1,3 +1,9 @@
 REM @USERINPUT: List of runs to process.
-
 call Impl\RunOneInstance.bat Run4Combi_2016_Referentie              /RunContext/GenerateAll
+
+
+
+call Impl\RunOneInstance.bat Run4Combi_2016_Referentie_Verhoging50cent              /RunContext/GenerateAll
+
+
+
