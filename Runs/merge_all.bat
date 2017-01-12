5dd8d6fef8805df4ec538f5f9def82f384e1c9e9
basvanbemmel <bas.vanbemmel@pbl.nl>
2017-01-12 07:41:56 +0100

    verbetering merge_all.bat met substitutie variabelen en extra weggeremde scenario's

--- Runs/merge_all.bat
+++ Runs/merge_all.bat
@@ -1,50 +1,64 @@
-call python linenumbers.py Run1 mut_allflow_Run1.csv mut_lijst
-call python linenumbers.py Run1 mut_allstock_Run1.csv mut_allstock_lijst
-call python linenumbers.py Run1 matrix_allflow_Run1.csv matrix_allflow
-call python linenumbers.py Run1 matrix_allstock_Run1.csv matrix_allstock
-call python linenumbers.py Run1 flow_Run1.csv NL
-call python linenumbers.py Run1 Gebiedsmaatregelen_Run1.csv gebiedsmaatregelen
-call python linenumbers.py Run1 Opwekking_Run1.csv opwekking
-call python linenumbers.py Run1 Verbetering_Run1.csv verbetering
-call python linenumbers.py Run1 mut_bebouwing_allflow_Run1.csv mut_bebouwing_allflow
-call python linenumbers.py Run1 mut_bebouwing_allstock_Run1.csv mut_bebouwing_allstock
-call python linenumbers.py Run2 mut_allflow_Run2.csv mut_lijst
-call python linenumbers.py Run2 mut_allstock_Run2.csv mut_allstock_lijst
-call python linenumbers.py Run2 matrix_allflow_Run2.csv matrix_allflow
-call python linenumbers.py Run2 matrix_allstock_Run2.csv matrix_allstock
-call python linenumbers.py Run2 flow_Run2.csv NL
-call python linenumbers.py Run2 Gebiedsmaatregelen_Run2.csv gebiedsmaatregelen
-call python linenumbers.py Run2 Opwekking_Run2.csv opwekking
-call python linenumbers.py Run2 Verbetering_Run2.csv verbetering
-call python linenumbers.py Run2 mut_bebouwing_allflow_Run2.csv mut_bebouwing_allflow
-call python linenumbers.py Run2 mut_bebouwing_allstock_Run2.csv mut_bebouwing_allstock
-call python linenumbers.py Run3 mut_allflow_Run3.csv mut_lijst
-call python linenumbers.py Run3 mut_allstock_Run3.csv mut_allstock_lijst
-call python linenumbers.py Run3 matrix_allflow_Run3.csv matrix_allflow
-call python linenumbers.py Run3 matrix_allstock_Run3.csv matrix_allstock
-call python linenumbers.py Run3 flow_Run3.csv NL
-call python linenumbers.py Run3 Gebiedsmaatregelen_Run3.csv gebiedsmaatregelen
-call python linenumbers.py Run3 Opwekking_Run3.csv opwekking
-call python linenumbers.py Run3 Verbetering_Run3.csv verbetering
-call python linenumbers.py Run3 mut_bebouwing_allflow_Run3.csv mut_bebouwing_allflow
-call python linenumbers.py Run3 mut_bebouwing_allstock_Run3.csv mut_bebouwing_allstock
-call python linenumbers.py Run4 mut_allflow_Run4.csv mut_lijst
-call python linenumbers.py Run4 mut_allstock_Run4.csv mut_allstock_lijst
-call python linenumbers.py Run4 matrix_allflow_Run4.csv matrix_allflow
-call python linenumbers.py Run4 matrix_allstock_Run4.csv matrix_allstock
-call python linenumbers.py Run4 flow_Run4.csv NL
-call python linenumbers.py Run4 Gebiedsmaatregelen_Run4.csv gebiedsmaatregelen
-call python linenumbers.py Run4 Opwekking_Run4.csv opwekking
-call python linenumbers.py Run4 Verbetering_Run4.csv verbetering
-call python linenumbers.py Run4 mut_bebouwing_allflow_Run4.csv mut_bebouwing_allflow
-call python linenumbers.py Run4 mut_bebouwing_allstock_Run4.csv mut_bebouwing_allstock
-call python linenumbers.py Run5 mut_allflow_Run5.csv mut_lijst
-call python linenumbers.py Run5 mut_allstock_Run5.csv mut_allstock_lijst
-call python linenumbers.py Run5 matrix_allflow_Run5.csv matrix_allflow
-call python linenumbers.py Run5 matrix_allstock_Run5.csv matrix_allstock
-call python linenumbers.py Run5 flow_Run5.csv NL
-call python linenumbers.py Run5 Gebiedsmaatregelen_Run5.csv gebiedsmaatregelen
-call python linenumbers.py Run5 Opwekking_Run5.csv opwekking
-call python linenumbers.py Run5 Verbetering_Run5.csv verbetering
-call python linenumbers.py Run5 mut_bebouwing_allflow_Run5.csv mut_bebouwing_allflow
-call python linenumbers.py Run5 mut_bebouwing_allstock_Run5.csv mut_bebouwing_allstock
+Set Run1=Run4Combi_2016_Referentie
+Set Run2=Run4Combi_2016_Referentie_Verhoging50cent
+
+REM Set Run3=NaamExtraScenario1
+REM Set Run4=NaamExtraScenario2
+REM Set Run5=NaamExtraScenario3
+
+call python linenumbers.py %Run1% mut_allflow_%Run1%.csv mut_lijst
+call python linenumbers.py %Run1% mut_allstock_%Run1%.csv mut_allstock_lijst
+call python linenumbers.py %Run1% matrix_allflow_%Run1%.csv matrix_allflow
+call python linenumbers.py %Run1% matrix_allstock_%Run1%.csv matrix_allstock
+call python linenumbers.py %Run1% flow_%Run1%.csv NL
+call python linenumbers.py %Run1% Gebiedsmaatregelen_%Run1%.csv gebiedsmaatregelen
+call python linenumbers.py %Run1% Opwekking_%Run1%.csv opwekking
+call python linenumbers.py %Run1% Verbetering_%Run1%.csv verbetering
+call python linenumbers.py %Run1% mut_bebouwing_allflow_%Run1%.csv mut_bebouwing_allflow
+call python linenumbers.py %Run1% mut_bebouwing_allstock_%Run1%.csv mut_bebouwing_allstock
+
+call python linenumbers.py %Run2% mut_allflow_%Run2%.csv mut_lijst
+call python linenumbers.py %Run2% mut_allstock_%Run2%.csv mut_allstock_lijst
+call python linenumbers.py %Run2% matrix_allflow_%Run2%.csv matrix_allflow
+call python linenumbers.py %Run2% matrix_allstock_%Run2%.csv matrix_allstock
+call python linenumbers.py %Run2% flow_%Run2%.csv NL
+call python linenumbers.py %Run2% Gebiedsmaatregelen_%Run2%.csv gebiedsmaatregelen
+call python linenumbers.py %Run2% Opwekking_%Run2%.csv opwekking
+call python linenumbers.py %Run2% Verbetering_%Run2%.csv verbetering
+call python linenumbers.py %Run2% mut_bebouwing_allflow_%Run2%.csv mut_bebouwing_allflow
+call python linenumbers.py %Run2% mut_bebouwing_allstock_%Run2%.csv mut_bebouwing_allstock
+
+REM call python linenumbers.py %Run3% mut_allflow_%Run3%.csv mut_lijst
+REM call python linenumbers.py %Run3% mut_allstock_%Run3%.csv mut_allstock_lijst
+REM call python linenumbers.py %Run3% matrix_allflow_%Run3%.csv matrix_allflow
+REM call python linenumbers.py %Run3% matrix_allstock_%Run3%.csv matrix_allstock
+REM call python linenumbers.py %Run3% flow_%Run3%.csv NL
+REM call python linenumbers.py %Run3% Gebiedsmaatregelen_%Run3%.csv gebiedsmaatregelen
+REM call python linenumbers.py %Run3% Opwekking_%Run3%.csv opwekking
+REM call python linenumbers.py %Run3% Verbetering_%Run3%.csv verbetering
+REM call python linenumbers.py %Run3% mut_bebouwing_allflow_%Run3%.csv mut_bebouwing_allflow
+REM call python linenumbers.py %Run3% mut_bebouwing_allstock_%Run3%.csv 
+
+REM call python linenumbers.py %Run4% mut_allflow_%Run4%.csv mut_lijst
+REM call python linenumbers.py %Run4% mut_allstock_%Run4%.csv mut_allstock_lijst
+REM call python linenumbers.py %Run4% matrix_allflow_%Run4%.csv matrix_allflow
+REM call python linenumbers.py %Run4% matrix_allstock_%Run4%.csv matrix_allstock
+REM call python linenumbers.py %Run4% flow_%Run4%.csv NL
+REM call python linenumbers.py %Run4% Gebiedsmaatregelen_%Run4%.csv gebiedsmaatregelen
+REM call python linenumbers.py %Run4% Opwekking_%Run4%.csv opwekking
+REM call python linenumbers.py %Run4% Verbetering_%Run4%.csv verbetering
+REM call python linenumbers.py %Run4% mut_bebouwing_allflow_%Run4%.csv mut_bebouwing_allflow
+REM call python linenumbers.py %Run4% mut_bebouwing_allstock_%Run4%.csv mut_bebouwing_allstock
+
+REM call python linenumbers.py %Run5% mut_allflow_%Run5%.csv mut_lijst
+REM call python linenumbers.py %Run5% mut_allstock_%Run5%.csv mut_allstock_lijst
+REM call python linenumbers.py %Run5% matrix_allflow_%Run5%.csv matrix_allflow
+REM call python linenumbers.py %Run5% matrix_allstock_%Run5%.csv matrix_allstock
+REM call python linenumbers.py %Run5% flow_%Run5%.csv NL
+REM call python linenumbers.py %Run5% Gebiedsmaatregelen_%Run5%.csv gebiedsmaatregelen
+REM call python linenumbers.py %Run5% Opwekking_%Run5%.csv opwekking
+REM call python linenumbers.py %Run5% Verbetering_%Run5%.csv verbetering
+REM call python linenumbers.py %Run5% mut_bebouwing_allflow_%Run5%.csv mut_bebouwing_allflow
+REM call python linenumbers.py %Run5% mut_bebouwing_allstock_%Run5%.csv mut_bebouwing_allstock
+
+
+
