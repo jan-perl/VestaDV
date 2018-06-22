64593aed6bdc47e4a70f442847b1d2977d5d44dc
basvanbemmel <bas.vanbemmel@pbl.nl>
2018-06-22 07:06:55 +0200

    aanroep 7.173 in batch PBL

--- batch/RunAll_PBL.bat
+++ batch/RunAll_PBL.bat
@@ -1,3 +1,3 @@
 Rem Test Debug Versie uit LocalData/bin
-Call Impl/RunImpl.bat R64 7173 N:\prog\ObjectVision\GeoDms7172\GeoDmsRun.exe
+Call Impl/RunImpl.bat R64 7173 N:\prog\ObjectVision\GeoDms7173\GeoDmsRun.exe
 
