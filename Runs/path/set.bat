fe460a70811cfcb8d40945a84d038876031173ac
jan-perl <jan-perl@hoogenraad.net>
2020-11-03 09:41:27 +0100

    Merge branch 'Vesta40' of https://github.com/RuudvandenWijngaart/VestaDV into VestaLeidraadOktober2019DHE
    # Conflicts:
    #	Runs/Referentie.dms
    #	Runs/batch_leidraad_datapakket.bat
    #	Runs/batch_leidraad_runs.bat
    #	Runs/batch_leidraad_runs_NL.bat
    #	Runs/batch_leidraad_statisch.bat
    #	Runs/batch_leidraad_totaal.bat
    #	Runs/path/set.bat
    #	model/stam/CalculationSchemes/Resultaten/NLResults.dms

--- Runs/path/set.bat
+++ Runs/path/set.bat
@@ -1,7 +1,13 @@
 REM aanpassingen voor machine specifieke paden
 
-set prj_dir=C:\Vesta\PD\VestaDV-VestaStartanalyse2020
-set log_dir=C:\Vesta\LD\VestaDV-VestaStartanalyse2020\log
+rem set prj_dir=C:\Vesta\PD\VestaDV-VestaStartanalyse2020
+rem set log_dir=C:\Vesta\LD\VestaDV-VestaStartanalyse2020\log
+
+rem set prj_dir=C:\Vesta\PD\VestaDV-VestaStartanalyse2020
+rem set log_dir=C:\Vesta\LD\VestaDV-VestaStartanalyse2020\log
+set prj_dir=d:\jhh\VestaMAIS\PD\VestaDV-LD2020DHE
+set log_dir=d:\jhh\VestaMAIS\LD\VestaDV-LD2020DHE\log
+
 set exe_dir=C:\Program Files\ObjectVision\GeoDms7206
 
 REM einde aanpassingen voor machine specifieke paden
