39aa25d1b35d590e9a87589fa09102fd38fb6d37
Jan Hoogenraad <jan-perl@hoogenraad.net>
2020-11-04 10:16:06 +0100

    andere computer

--- Runs/path/set.bat
+++ Runs/path/set.bat
@@ -5,8 +5,8 @@ rem set log_dir=C:\Vesta\LD\VestaDV-VestaStartanalyse2020\log
 
 rem set prj_dir=C:\Vesta\PD\VestaDV-VestaStartanalyse2020
 rem set log_dir=C:\Vesta\LD\VestaDV-VestaStartanalyse2020\log
-set prj_dir=d:\jhh\VestaMAIS\PD\VestaDV-LD2020DHE
-set log_dir=d:\jhh\VestaMAIS\LD\VestaDV-LD2020DHE\log
+set prj_dir=e:\jhh\VestaMAIS\PD\VestaDV-LD2020DHE
+set log_dir=e:\jhh\VestaMAIS\LD\VestaDV-LD2020DHE\log
 
 set exe_dir=C:\Program Files\ObjectVision\GeoDms7206
 
